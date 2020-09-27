ef70ced9357dbd8a9d6650da546b51d0fae9ca4d
Autosoft <xtianm4@gmail.com>
2020-09-28 00:27:38 +0300

    backup

--- src/screens/AddNew.js
+++ src/screens/AddNew.js
@@ -1,14 +1,52 @@
 import React, {useState} from 'react';
-import {View, StyleSheet, TextInput, TouchableOpacity, ScrollView, Platform} from 'react-native';
+import {View, StyleSheet, TextInput, TouchableOpacity, ScrollView, Platform, SafeAreaView} from 'react-native';
 import {
     Picker,
     FormInput,
 } from "@99xt/first-born";
+import MultiSelect from 'react-native-multiple-select';
 
 const AddNew = ({navigation}) => {
 
+    const items = [{
+        id: '92iijs7yta',
+        name: 'Ondo',
+    }, {
+        id: 'a0s0a8ssbsd',
+        name: 'Ogun',
+    }, {
+        id: '16hbajsabsd',
+        name: 'Calabar',
+    }, {
+        id: 'nahs75a5sg',
+        name: 'Lagos',
+    }, {
+        id: '667atsas',
+        name: 'Maiduguri',
+    }, {
+        id: 'hsyasajs',
+        name: 'Anambra',
+    }, {
+        id: 'djsjudksjd',
+        name: 'Benue',
+    }, {
+        id: 'sdhyaysdj',
+        name: 'Kaduna',
+    }, {
+        id: 'suudydjsjd',
+        name: 'Abuja',
+    }];
+
     const [amount, setAmount] = useState('');
     const [currency, setCurrency] = useState('');
+    const [selectedItems, setSelectedItems] = useState(items);
+
+    // state = {selectedItems = [];};
+
+
+    const onSelectedItemsChange = selectedItems => {
+        setSelectedItems(selectedItems);
+    };
 
     return (
         <View style={styles.container}>
@@ -27,7 +65,30 @@ const AddNew = ({navigation}) => {
                         <Picker.Item value="Female" label="Female"/>
                     </Picker>
                 </View>
-
+                <View style={{ marginLeft: 5, flex: 1 }}>
+                    <MultiSelect
+                        hideTags
+                        items={items}
+                        uniqueKey="id"
+                        // ref={(component) => { multiSelect = component }}
+                        onSelectedItemsChange={onSelectedItemsChange}
+                        selectedItems={selectedItems}
+                        selectText="Diseases and symptoms"
+                        searchInputPlaceholderText="Search Diseases..."
+                        onChangeInput={ (text)=> console.log(text)}
+                        altFontFamily="ProximaNova-Light"
+                        tagRemoveIconColor="#CCC"
+                        tagBorderColor="#CCC"
+                        tagTextColor="#CCC"
+                        selectedItemTextColor="#CCC"
+                        selectedItemIconColor="#CCC"
+                        itemTextColor="#000"
+                        displayKey="name"
+                        searchInputStyle={{ color: '#CCC' }}
+                        submitButtonColor="#CCC"
+                        hideSubmitButton={true}
+                    />
+                </View>
             </ScrollView>
         </View>
     );
