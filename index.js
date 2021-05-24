eb4a4a362c3c06d3c361f6cac3be0354ccee7484
Lillian26 <lilliannamusisi@gmail.com>
2021-05-24 16:53:33 +0300

    Added actions for Patient model

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 import { Platform } from 'react-native'
 import { Database } from '@nozbe/watermelondb'
 import SQLiteAdapter from '@nozbe/watermelondb/adapters/sqlite'
@@ -19,27 +19,28 @@ import ConditionStatus from './src/model/ConditionStatus'
 
 // First, create the adapter to the underlying database:
 const adapter = new SQLiteAdapter({
-    schema,
-    // (You might want to comment it out for development purposes -- see Migrations documentation)
-    migrations,
-    // (optional database name or file system path)
-    // dbName: 'myapp',
-    // (recommended option, should work flawlessly out of the box on iOS. On Android,
-    // additional installation steps have to be taken - disable if you run into issues...)
-    jsi: true, /* Platform.OS === 'ios' */
-    // (optional, but you should implement this method)
-    onSetUpError: error => {
-      // Database failed to load -- offer the user to reload the app or log out
-    }
-  })
-  
-  // Then, make a Watermelon database from it!
-  const database = new Database({
-    adapter,
-    modelClasses: [Case, Patient, Condition, ConditionStatus
-      // Post, // ⬅️ You'll add Models to Watermelon here
-    ],
-    actionsEnabled: true,
-  })
+  schema,
+  // (You might want to comment it out for development purposes -- see Migrations documentation)
+  migrations,
+  // (optional database name or file system path)
+  dbName: 'mohdb',
+  // (recommended option, should work flawlessly out of the box on iOS. On Android,
+  // additional installation steps have to be taken - disable if you run into issues...)
+  jsi: true, /* Platform.OS === 'ios' */
+  // (optional, but you should implement this method)
+  onSetUpError: error => {
+    // Database failed to load -- offer the user to reload the app or log out
+    alert("Failed to load data. Log out and Login again.")
+  }
+})
+
+// Then, make a Watermelon database from it!
+export const database = new Database({
+  adapter,
+  modelClasses: [Case, Patient, Condition, ConditionStatus
+    // Post, // ⬅️ You'll add Models to Watermelon here
+  ],
+  actionsEnabled: true,
+})
 
 AppRegistry.registerComponent(appName, () => App);
--- src/model/Case.js
+++ src/model/Case.js
@@ -2,16 +2,16 @@ import { Model } from '@nozbe/watermelondb'
 import { field, readonly, date, relation, children } from '@nozbe/watermelondb/decorators'
 
 export default class Case extends Model {
-    static table = 'cases';
-    static associations = {
-        condition_statuses: {type: "has_many", foreignKey: "case_id"}
-      };
-      
-      @field('schoolId') schoolId;
-      @field('condition_status_id') condition_status_id;
-      @readonly @date('created_at') createdAt;
-      @readonly @date('updated_at') updatedAt;
-  
-      @relation('pateints', 'patient_id') patient;
-      @children('condition_statuses') condition_statuses
-  }
+  static table = 'cases';
+  static associations = {
+    condition_statuses: { type: "has_many", foreignKey: "case_id" }
+  };
+
+  @field('schoolId') schoolId;
+  @field('condition_status_id') condition_status_id;
+  @readonly @date('created_at') createdAt;
+  @readonly @date('updated_at') updatedAt;
+
+  @relation('pateints', 'patient_id') patient;
+  @children('condition_statuses') condition_statuses
+}
