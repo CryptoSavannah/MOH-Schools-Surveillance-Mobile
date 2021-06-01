9887a4ed509231e1ed0a279145a5c1da39f39fdd
Lillian26 <lilliannamusisi@gmail.com>
2021-06-01 19:03:57 +0300

    Refined New Case

--- src/components/MenuCard.js
+++ src/components/MenuCard.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import { FlatList } from 'react-native';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Alert } from 'react-native';
 import { Card, Divider } from 'react-native-elements';
 import * as theme from '../constants/theme';
-import { useTheme } from '@react-navigation/native';
 
 
 const MenuCard = (props) => {
@@ -13,43 +12,52 @@ const MenuCard = (props) => {
   function Item({ title, number }) {
     return (
       <TouchableOpacity
-        onPress={() => props.navigation.navigate('Pending')}
+        onPress={() => {
+
+          Alert.alert(
+            title, 'View this Case?',
+            [
+              {
+                text: "Cancel",
+                onPress: () => console.log("Cancel Pressed"),
+                style: "cancel"
+              },
+              {
+                text: "OK", onPress: () => {
+                  // props.navigation.navigate('Pending')
+                  console.log("OK Pressed")
+                }
+              }
+            ]
+          );
+        }}
         style={[styles.item, { alignItems: 'center', justifyContent: 'flex-start', flexDirection: 'row' }]}
       >
-        {/* <View style={[styles.item, {alignItems: 'center', justifyContent: 'center'}]}> */}
-        {/* <Text style={styles.title}>{title}</Text> */}
-        {/* <Icon distance /> */}
         <Text h3 style={{ flex: 1 }}>{number}</Text>
         <Text paragraph color="gray">{title}</Text>
-        {/* </View> */}
       </TouchableOpacity>
     );
-  } 
+  }
   return (
 
     <Card containerStyle={styles.card}>
       <View style={styles.container}>
-      <View style={styles.titleView}>
-          {/*<Text style={styles.title}>{'Menu title'}</Text>*/}
+        <View style={styles.titleView}>
           <Text style={styles.sector}>{menutab.title}</Text>
         </View>
         <View>
-           <FlatList
+          <FlatList
             pagingEnabled={true}
-            // showsHorizontalScrollIndicator={false}
             legacyImplementation={true}
             data={menutab.items}
             renderItem={({ item }) => <Item title={item.title} number={item.number} />}
             keyExtractor={item => item.id}
-            // style={{ width: SCREEN_WIDTH + 5, height: '50%', marginHorizontal: 5, flex: 1 }}
           />
         </View>
       </View>
 
       <Divider style={{ backgroundColor: '#fff', marginVertical: 10 }} />
-      <TouchableOpacity
-        onPress={() => onOpen(menutab.page)}
-      >
+      <TouchableOpacity onPress={() => onOpen(menutab.page)} >
         <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
           <Text style={styles.notes}>{'View more'}</Text>
         </View>
@@ -62,31 +70,21 @@ export default MenuCard;
 
 const styles = StyleSheet.create({
   card: {
-    // backgroundColor: 'rgba(56, 172, 236, 1)',
-    // flex: 1,
-    // borderWidth: 0,
     borderRadius: 10,
-    // width: '100%',
   },
   container: {
-    // flex: 1,
-    // flexDirection: 'row',
-    // justifyContent: 'space-between',
     alignItems: 'center'
   },
   titleView: {
     flex: 5,
-    // marginLeft: 15,
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-    // color: '#fff'
   },
   notes: {
     fontSize: 15,
     color: '#99A3A4',
-    // textTransform: 'capitalize'
   },
   sector: {
     fontSize: 15,
@@ -102,12 +100,7 @@ const styles = StyleSheet.create({
     borderTopColor: '#fff',
     borderRightColor: '#fff',
     borderWidth: 0.5,
-    // shadowColor: theme.colors.shadow,
-    // shadowOpacity: 1,
-    // shadowRadius: 1,
-    // shadowOffset: { width: 0, height: 0 },
-    // elevation: 2,
     padding: 10,
     width: 300
-}
+  }
 });
