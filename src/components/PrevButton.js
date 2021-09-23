521645d3cf5b27649a85b134739922018c30095a
Lillian26 <lilliannamusisi@gmail.com>
2021-09-23 18:30:29 +0300

    Refined UI, form builder for all forms

--- src/components/PrevButton.js
+++ src/components/PrevButton.js
@@ -1,30 +1,36 @@
-import {StyleSheet, View} from 'react-native';
-import {Button, Input, Text} from '@99xt/first-born';
+import { StyleSheet, View } from 'react-native';
+import { Button, Input, Text } from '@99xt/first-born';
 import React from 'react';
 import { colors } from '../constants/theme';
 
-const PrevButton = ({goToPrev}) => {
+const PrevButton = ({ goToPrev }) => {
 
     return (
-        <View style={{ alignItems: 'flex-end' }}>
-            <View style={{ width: 80 }}>
-                <Button
-                    rounded
-                    block
-                    // style={styles.btn}
-                    color={colors.caption} title="Prev" onPress={() => goToPrev()}>
-                    {/*color="rgba(3, 136, 229, 1)" title="Next" onPress={() => wizard.current.next()}>*/}
-                    <Text>Back</Text>
-                </Button>
-            </View>
+        // <View style={{ alignItems: 'flex-end' }}>
+        //     <View style={{ width: 80 }}>
+        //         <Button
+        //             rounded
+        //             block
+        //             // style={styles.btn}
+        //             color={colors.caption} title="Prev" onPress={() => goToPrev()}>
+        //             {/*color="rgba(3, 136, 229, 1)" title="Next" onPress={() => wizard.current.next()}>*/}
+        //             <Text>Back</Text>
+        //         </Button>
+        //     </View>
+        // </View>
+
+        <View style={{ width: 80, alignItems: 'flex-end' }}>
+            <Button
+                block
+                // style={styles.btn}
+                // color="grey" title="Cancel" onPress={() => { cancel() }} 
+                color="grey" title="Prev" onPress={() => goToPrev()}>
+                <Text>Back</Text>
+            </Button>
+
         </View>
 
     );
 };
 
 export default PrevButton;
-
-
-const styles = StyleSheet.create({
-    action: {},
-});
--- src/screens/AddNew.js
+++ src/screens/AddNew.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, TextInput, Text, SafeAreaView, Button, Dimensions } from 'react-native';
+import { View, StyleSheet, Text, SafeAreaView, Button, ActivityIndicator } from 'react-native';
 import {
   FormInput,
 } from "@99xt/first-born";
@@ -19,6 +19,10 @@ import { Picker } from '@react-native-picker/picker';
 import realm, {
   addPatient
 } from "../database/database";
+import { useController, useForm } from 'react-hook-form';
+import { Checkbox as CheckboxP, List as ListP, TextInput } from 'react-native-paper';
+import { FormBuilder } from 'react-native-paper-form-builder';
+import { fetchChronicConditions, fetchDisabilities } from '../model/data';
 
 const AddNew = ({ route, navigation }) => {
 
@@ -35,6 +39,9 @@ const AddNew = ({ route, navigation }) => {
   const [selectedDisability, setSelectedDisability] = useState('');
   const [selectedIllness, setSelectedIllness] = useState('');
   const [otherMedicalCondition, setOtherMedicalCondition] = useState('');
+  const [conditions, setConditions] = useState([]);
+  const [disabilities, setDisabilities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const { nin } = route.params ?? {};
   const { fnameR } = route.params ?? {};
@@ -43,6 +50,20 @@ const AddNew = ({ route, navigation }) => {
   const { dobR } = route.params ?? {};
   const { immunizationStatusR } = route.params ?? {};
   const { disabilityR } = route.params ?? {};
+
+  const { control, setFocus, handleSubmit } = useForm({
+    defaultValues: {
+      firstName: '',
+      lastName: '',
+      gender: '',
+      date_of_birth: '',
+      nin: '',
+      immunization_status: '',
+      disability: '',
+      chronic_condiction: ''
+    },
+    mode: 'onChange',
+  });
 
   useEffect(() => {
     if (typeof fnameR !== 'undefined') {
@@ -84,7 +105,39 @@ const AddNew = ({ route, navigation }) => {
       })
       .catch(err => console.log(err));
 
-  });
+          //fetch diseases
+    fetchChronicConditions().then(res => {
+      // console.log(res)
+      let conds = [];
+
+      res.data.map(x => {
+        conds.push({
+          value: x.id,
+          label: x.condition_name,
+        });
+      });
+
+      setConditions(conds)
+
+    })
+
+    fetchDisabilities().then(res => {
+      // console.log(res)
+      let disabltis = [];
+
+      res.data.map(x => {
+        disabltis.push({
+          value: x.id,
+          label: x.disability_name,
+        });
+      });
+
+      setDisabilities(disabltis);
+      console.log(disabilities);
+
+    })
+
+  },[]);
 
   // useFocusEffect(
   //     React.useCallback(() => {
@@ -144,37 +197,16 @@ const AddNew = ({ route, navigation }) => {
     return string.split(' ').join('');
   };
 
-  const savePatient = async() => {
+  const savePatient = handleSubmit(async (data) => {
+    setIsLoading(true)
+    console.log(data);
 
-    // if (isNINAvailable) {
-    //   data.isValidNIN = !(removeSpaces(idNum) === "");
+    setTimeout(() => {
+      setIsLoading(false)
+      alert("Patient Information saved.");
+      cancel()
+    }, 1000);
 
-    //   //step 1. check for required fields: idNum
-    //   if (!(removeSpaces(idType) === "") && data.isValidNIN) {
-
-    //     JSHash(idNum, CONSTANTS.HashAlgorithms.sha256)
-    //       .then(hash => console.log(hash))
-    //       .catch(
-    //         // e => console.log(e)
-    //         );
-
-    //     alert("Case has been Recorded");
-
-    //     clearState();
-    //   } else {
-    //     alert("Fill in all the fields");
-    //   }
-
-    // } else {
-
-    //step 1. check for required fields: fname, lname, gender, dob
-    data.isValidFName = !(removeSpaces(fname) === "");
-    data.isValidLName = !(removeSpaces(lname) === "");
-    data.isValidGender = !(removeSpaces(gender) === "");
-    data.isValidDob = !(removeSpaces(dob) === "");
-    data.isValidNIN = !(removeSpaces(idNum) === "");
-
-    if (data.isValidFName && data.isValidLName && data.isValidGender && data.isValidDob && data.isValidNIN) {
       var stdID = idNum;
       JSHash(stdID, CONSTANTS.HashAlgorithms.sha256)
         .then(hash => {
@@ -194,7 +226,7 @@ const AddNew = ({ route, navigation }) => {
             method: 'post',
             url: CREATE_PATIENT_KEY,
             headers: {
-              'Authorization': `Bearer ${userToken}`
+              'Authorization': `Bearerselect ${userToken}`
             },
             data: data
           };
@@ -234,12 +266,12 @@ const AddNew = ({ route, navigation }) => {
 
           //then reroute to the new case, with std object (nin, name) 
           //to get serverId for the case in the next screen
-          navigation.navigate("NewCase", {
-            nin: idNum,
-            name: `${fname} ${lname}`,
-            genderR: gender,
-            dobR: dob
-          })
+          // navigation.navigate("NewCase", {
+          //   nin: idNum,
+          //   name: `${fname} ${lname}`,
+          //   genderR: gender,
+          //   dobR: dob
+          // })
 
           //store other patient in storage eg async storage or realm
 
@@ -256,159 +288,182 @@ const AddNew = ({ route, navigation }) => {
         .catch(
           e => console.log("Hashing Catch: " + e)
         );
-    }
-    else {
-      alert("Fill in all the fields");
-    }
     // }
-  };
+  });
 
   const cancel = () => {
     //clear fields, back to home
-    clearState();
     navigation.navigate("Home");
-  };
-
-  const clearState = () => {
-    setFName(''); setLName(''); setDob(''); setIDNum(''); setGender('');
-    data.isValidNIN = data.isValidLName = data.isValidFName = data.isValidGender = data.isValidDob = true;
-  };
+  }
 
 
   return (
     <View style={styles.container}>
       <SafeAreaView>
-        <View style={{ position: 'absolute', top: '-5%', alignSelf: 'center' }}>
-          <Text style={{ paddingVertical: 20, fontWeight: "bold" }}>
-            {"Enter The Patient's Details:".toUpperCase()}
-          </Text>
-        </View>
-
-        <ScrollView style={{ paddingTop: 20 }}>
-          <View style={[styles.action, { paddingTop: 3 }]}>
-            <TextInput style={{ fontSize: 18, width: '100%' }} label="First name" placeholder="First name" onChangeText={(val) => { setFName(val) }}
-              value={fname} />
-          </View>
-          <View style={[styles.action, { paddingTop: 3 }]}>
-            <TextInput style={{ fontSize: 18, width: '100%' }} label="Last name" placeholder="Last name" onChangeText={(val) => { setLName(val); }}
-              value={lname} />
-          </View>
-          <View style={[styles.view, { paddingTop: 10, paddingBottom: 3 }]}>
-            <Text style={{ fontSize: 18, color: '#808080', marginRight: '2%' }}>Gender : </Text>
-            <TouchableOpacity onPress={() => setGender('M')} style={[styles.view, { paddingHorizontal: "4%" }]}>
-              <Text style={{ fontSize: 17 }}>Male</Text>
-              <RadioButton value="M" status={gender === 'M' ? 'checked' : 'unchecked'} color="purple" onPress={() => setGender('M')} />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => setGender('F')} style={[styles.view, { paddingHorizontal: "4%" }]}>
-              <Text style={{ fontSize: 17 }}>Female</Text>
-              <RadioButton value="F" color="purple" status={gender === 'F' ? 'checked' : 'unchecked'} onPress={() => setGender('F')} />
-            </TouchableOpacity>
-          </View>
-
-          <View style={[styles.action, {}]}>
-            <TextInput style={{ fontSize: 18, width: '100%' }} onFocus={showDatePicker} onKeyPress={showDatePicker} label="Date of Birth" placeholder="Enter Date of Birth"
-              value={dob == '' ? '' : `Date of Birth:  ${dob}`} showSoftInputOnFocus={false} />
-          </View>
-          <DateTimePickerModal
-            isVisible={isDatePickerVisible}
-            mode="date"
-            onConfirm={handleConfirm}
-            onCancel={hideDatePicker}
-          />
-          <View style={[styles.action, {}]}>
-            <TextInput style={{ fontSize: 18, width: '100%', paddingTop: 0 }} label="NIN" placeholder="NIN" onChangeText={(val) => setIDNum(val)} value={idNum} />
-          </View>
-          <View style={[styles.action2, { height: 50, marginVertical: 5, width: '100%', alignSelf: 'center' }]} >
-            <Picker style={{
-              color: selectedImmunizationStatus === '' ? '#A9A9A9' : '#000', height: '100%', width: '90%', fontSize: 18, fontWeight: '100',
-              transform: [{ scaleX: 1.12 }, { scaleY: 1.12 }], left: '4%', position: 'absolute',
-            }} selectedValue={selectedImmunizationStatus}
-              onValueChange={(itemValue, itemIndex) => setSelectedImmunizationStatus(itemValue)} itemStyle={{ fontSize: 18 }} >
-              <Picker.Item value="" label="Immunization Status:" />
-              <Picker.Item value="F" label="Fully Immunized" />
-              <Picker.Item value="P" label="Partially Immunized" />
-              <Picker.Item value="N" label="Not Immunized" />
-            </Picker>
-          </View>
-          <View style={[styles.action2, { height: 50, marginVertical: 5, width: '100%', alignSelf: 'center' }]} >
-            <Picker style={{
-              color: selectedDisability === '' ? '#A9A9A9' : '#000', height: '100%', width: '90%', fontSize: 18, fontWeight: '100',
-              transform: [{ scaleX: 1.12 }, { scaleY: 1.12 }], left: '4%', position: 'absolute',
-            }} selectedValue={selectedDisability}
-              onValueChange={(itemValue, itemIndex) => setSelectedDisability(itemValue)} itemStyle={{ fontSize: 18 }} >
-              <Picker.Item value="" label="Disability:" />
-              <Picker.Item value="0" label="None" />
-              <Picker.Item value="1" label="Difficulty in seeing" />
-              <Picker.Item value="2" label="Albinism" />
-              <Picker.Item value="3" label="Difficulty in hearing" />
-              <Picker.Item value="4" label="Delayed age specific motor development" />
-              <Picker.Item value="5" label="Delayed age specific height for age (dwarfism)" />
-              <Picker.Item value="6" label="Difficulty understanding" />
-              <Picker.Item value="7" label="Difficulty in remembering" />
-              <Picker.Item value="8" label="Difficulty in writing" />
-              <Picker.Item value="9" label="Difficulty washing all over or dressing" />
-              <Picker.Item value="10" label="Mentally impared" />
-              <Picker.Item value="11" label="Emotionally impared" />
-            </Picker>
-          </View>
-
-          <View style={[styles.action2, { height: 50, marginVertical: 5, width: '100%', alignSelf: 'center' }]} >
-            <Picker style={{
-              color: selectedIllness === '' ? '#A9A9A9' : '#000', height: '100%', width: '90%', fontSize: 18, fontWeight: '100',
-              transform: [{ scaleX: 1.12 }, { scaleY: 1.12 }], left: '4%', position: 'absolute',
-            }} selectedValue={selectedIllness}
-              onValueChange={(itemValue, itemIndex) => {
-                setSelectedIllness(itemValue);
-              }} itemStyle={{ fontSize: 18 }} >
-              <Picker.Item value="" label="Chronic medical condition/allergies:" />
-              {/* {conditions.map(x => {
-                <Picker.Item value={x.id} label={x.name} key={x.id}/>
-              })
-              } */}
-              <Picker.Item value="1" label="Asthma" />
-              <Picker.Item value="2" label="Sickle cell disease" />
-              <Picker.Item value="3" label="TB on treatment" />
-              <Picker.Item value="4" label="Cancer" />
-              <Picker.Item value="5" label="Epilepsy" />
-              <Picker.Item value="6" label="Chronic / congenital heart disease" />
-              <Picker.Item value="7" label="Mental disorder" />
-              <Picker.Item value="8" label="Diabetes" />
-              <Picker.Item value="9" label="HIV/AIDS" />
-              <Picker.Item value="10" label="Food/ medicine allergies" />
-              <Picker.Item value="11" label="Other medical conditions/ allergies" />
-              <Picker.Item value="12" label="Covid" />
-            </Picker>
-          </View>
-
-          {(selectedIllness === "10" || selectedIllness === "11") ? <View style={[styles.action, { marginBottom: 10, width: '100%', alignSelf: 'center', paddingTop: 3 }]}>
-            <TextInput style={{ fontSize: 18, width: '100%' }} label="Specify medical condition" placeholder="Specify medical condition:"
-              onChangeText={(val) => { setOtherMedicalCondition(val); }} value={otherMedicalCondition} keyboardType="text" />
-          </View> : null}
-
-          <View style={{
+      <ScrollView style={{ paddingTop: 15 }} >
+        <FormBuilder
+          control={control}
+          setFocus={setFocus}
+          formConfigArray={[
+            {
+              name: 'firstname',
+              type: 'text',
+              textInputProps: {
+                label: 'First name',
+              },
+              rules: {
+                required: {
+                  value: true,
+                  message: 'First name is required',
+                },
+              },
+            },
+            {
+              name: 'lastname',
+              type: 'text',
+              textInputProps: {
+                label: 'Last Name',
+              },
+              rules: {
+                required: {
+                  value: true,
+                  message: 'Last Name is required',
+                },
+              },
+            },
+            {
+              name: 'gender',
+              type: 'select',
+              textInputProps: {
+                label: 'Gender',
+              },
+              rules: {
+                required: {
+                  value: true,
+                  message: 'Gender is required',
+                },
+              },
+              options: [
+                {
+                  value: 0,
+                  label: 'Female',
+                },
+                {
+                  value: 1,
+                  label: 'Male',
+                },
+              ],
+            },
+            {
+              name: 'date_of_birth',
+              type: 'text',
+              textInputProps: {
+                label: 'Date of birth',
+              },
+              rules: {
+                required: {
+                  value: true,
+                  message: 'Date of birth is required',
+                },
+              },
+            },
+            {
+              name: 'nin',
+              type: 'text',
+              textInputProps: {
+                label: 'NIN',
+              },
+              rules: {
+                required: {
+                  value: true,
+                  message: 'NIN is required',
+                },
+              },
+            },
+            {
+              name: 'immunization_status',
+              type: 'select',
+              textInputProps: {
+                label: 'Immunization Status',
+              },
+              rules: {
+                required: {
+                  value: true,
+                  message: 'Immunisation Status is required',
+                },
+              },
+              options: [
+                {
+                  label: 'Fully Immunized',
+                  value: 1,
+                },
+                {
+                  label: 'Partially Immunized',
+                  value: 2,
+                },
+                {
+                  label: 'Not Immunized',
+                  value: 3,
+                },
+              ],
+            },
+            {
+              name: 'disability',
+              type: 'autocomplete',
+              textInputProps: {
+                label: 'Disability',
+              },
+              rules: {
+                required: {
+                  value: true,
+                  message: 'Disability Status is required',
+                },
+              },
+              options: disabilities,
+            },
+            {
+              name: 'chronic_condition',
+              type: 'autocomplete',
+              textInputProps: {
+                label: 'Chronic medical condition/allergies:',
+              },
+              rules: {
+                required: {
+                  value: true,
+                  message: 'Condition is required',
+                },
+              },
+              options: conditions
+            },
+          ]}
+        />
+        <View style={{
             flexDirection: 'row', justifyContent: 'space-between', paddingVertical: 5,
             paddingTop: 25, marginBottom: 10
           }}>
             <View style={{ width: 80, marginBottom: 10 }}>
-              <Button rounded
+              <Button
                 block
-                style={styles.btn}
                 color="grey" title="Cancel" onPress={() => { cancel() }} />
 
             </View>
             <View style={{ width: 80, marginBottom: 10 }}>
-              <Button title="Register"
-                rounded
+              {!isLoading ? <Button title="Register"
                 block
-                style={styles.btn}
                 color="rgba(3, 136, 229, 1)"
                 onPress={() => { savePatient() }}
               >
               </Button>
+                :
+                <TouchableOpacity
+                  style={[, styles.btn, { alignItems: "center", padding: 8, backgroundColor: "rgba(3, 136, 229, 1)" }]}
+                  underlayColor='rgba(3, 136, 229, 1)'
+                >
+                  <ActivityIndicator animating={isLoading} color="#fff" />
+                </TouchableOpacity>}
             </View>
           </View>
-
-        </ScrollView>
+          </ScrollView>
       </SafeAreaView>
     </View>
   );
@@ -465,3 +520,28 @@ const styles = StyleSheet.create({
   }
 
 });
+
+// function dobPicker(props) {
+//   const { name, rules, shouldUnregister, defaultValue, control } = props;
+//   const { field } = useController({
+//     name,
+//     rules,
+//     shouldUnregister,
+//     defaultValue,
+//     control,
+//   });
+
+//   return (
+//     <View>
+//       <View style={[styles.action, {}]}>
+//         <TextInput style={{ fontSize: 18, width: '100%' }} onFocus={showDatePicker} onKeyPress={showDatePicker} label="Date of Birth" placeholder="Enter Date of Birth"
+//           value={field.value == '' ? '' : `Date of Birth:  ${field.value}`} showSoftInputOnFocus={false} />
+//       </View>
+//       <DateTimePickerModal
+//         isVisible={isDatePickerVisible}
+//         mode="date"
+//         onConfirm={handleConfirm}
+//         onCancel={hideDatePicker}
+//       /></View>
+//   );
+// }
